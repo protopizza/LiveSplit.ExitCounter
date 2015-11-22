8ca7de9ab63f051cc4df4b2e092c0d508e4746d2
Sergey Papushin <spapushin@gmail.com>
2015-11-21 19:12:39 -0600

    Change to .NET 4.5 and allow updates

--- UI/Components/CounterComponentFactory.cs
+++ UI/Components/CounterComponentFactory.cs
@@ -5,44 +5,20 @@ namespace LiveSplit.UI.Components
 {
     public class CounterComponentFactory : IComponentFactory
     {
-        public string ComponentName
-        {
-            get { return "Counter"; }
-        }
+        public string ComponentName => "Counter";
 
-        public string Description
-        {
-            get { return "A labelled counter, allowing an initial value to be incremented/decremented by a specified amount."; }
-        }
+        public string Description => "A labelled counter, allowing an initial value to be incremented/decremented by a specified amount.";
 
-        public ComponentCategory Category
-        {
-            get { return ComponentCategory.Other; }
-        }
+        public ComponentCategory Category => ComponentCategory.Other;
 
-        public IComponent Create(LiveSplitState state)
-        {
-            return new CounterComponent(state);
-        }
+        public IComponent Create(LiveSplitState state) => new CounterComponent(state);
 
-        public string UpdateName
-        {
-            get { return ComponentName; }
-        }
+        public string UpdateName => ComponentName;
 
-        public string XMLURL
-        {
-            get { return string.Empty; }
-        }
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Counter.xml";
 
-        public string UpdateURL
-        {
-            get { return string.Empty; }
-        }
+        public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version
-        {
-            get { return Version.Parse("1.0"); }
-        }
+        public Version Version => Version.Parse("1.0");
     }
 }
